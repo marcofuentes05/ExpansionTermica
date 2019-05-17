5b459f600fa03bf64721486c3e021891725a35fa
Marco Fuentes <marcofuentes1607@gmail.com>
2019-05-16 19:50:46 -0600

    Primer Commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraScript : MonoBehaviour
+{
+    public Slider Li;
+    public Slider AT;
+    public Text LiT;
+    public Text ATT;
+    public Text coeficiente;
+    public Text longitudFinal;
+    public GameObject cilindro;
+
+    int material;
+    float lf;
+    float Al; //El delta L
+
+    private float cAluminio = 0.000024f;
+    private float cLaton    = 0.000020f;
+    private float cCobre    = 0.000017f;
+    private float cVidrio   = 0.000070f; //Es el promedio
+    private float cCuarzo   = 0.000004f;
+    private float cAcero    = 0.000012f;
+
+    //Para saber cuando animar la barra
+    public bool animation = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LiT.gameObject.GetComponent<Text>().text = Li.value + " m";
+        ATT.gameObject.GetComponent<Text>().text = AT.value + " K";
+
+        cilindro.gameObject.GetComponent<Transform>().localScale = new Vector3(1, Li.value, 1);
+
+        if (animation)
+        {
+            cilindro.gameObject.GetComponent<Transform>().localScale += new Vector3(0,Al*100,0);
+        }
+        if (cilindro.gameObject.GetComponent<Transform>().localScale == new Vector3 (1,30,1)){
+            animation = false;
+        }
+    }
+
+    public void setCoeficiente(int numero)
+    {
+        material = numero;
+        if (numero == 0)
+        {
+            coeficiente.gameObject.GetComponent<Text>().text = "="+cAcero.ToString()+" /°C";
+        }
+        else if (numero == 1)
+        {
+            coeficiente.gameObject.GetComponent<Text>().text = "=" + cAluminio.ToString() + " /°C";
+        }
+        else if (numero == 2)
+        {
+            coeficiente.gameObject.GetComponent<Text>().text = "=" + cCobre.ToString() + " /°C";
+        }
+        else if (numero == 3)
+        {
+            coeficiente.gameObject.GetComponent<Text>().text = "=" + cCuarzo.ToString() + " /°C";
+        }
+        else if (numero == 4)
+        {
+            coeficiente.gameObject.GetComponent<Text>().text = "=" + cLaton.ToString() + " /°C";
+        }
+        else if (numero == 5)
+        {
+            coeficiente.gameObject.GetComponent<Text>().text = "=" + cVidrio.ToString() + " /°C";
+        }
+    }
+
+    //TODO el codigo onClick() del boton de Aplicar
+    public void onClick()
+    {
+        if (material == 0)
+        {
+            lf = Li.value * AT.value * cAcero;
+        }
+        else if (material == 1)
+        {
+            lf = Li.value * AT.value * cAluminio;
+        }
+        else if (material == 2)
+        {
+            lf = Li.value * AT.value * cCobre;
+        }
+        else if (material == 3)
+        {
+            lf = Li.value * AT.value * cCuarzo;
+        }
+        else if (material == 4)
+        {
+            lf = Li.value * AT.value * cLaton;
+        }
+        else if (material == 5)
+        {
+            lf = Li.value * AT.value * cVidrio;
+        }
+        Al = lf;
+        lf += Li.value;
+
+        animation = true;
+
+        
+
+        string text = " = " + lf.ToString();
+
+        longitudFinal.gameObject.GetComponent<Text>().text = text;
+    }
+}
---
+++ Assets/Scripts/VaraScript.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VaraScript : MonoBehaviour
+{
+    public Canvas canvas;
+
+    //Texturas de los materiales
+    public Texture acero;
+    public Texture aluminio;
+    public Texture laton;
+    public Texture cobre;
+    public Texture vidrio;
+    public Texture invar;
+    public Texture cuarzo;
+
+    //Coeficientes de expansión lineal de los materiales (alfa) en (°C^-1)
+    private float cAluminio = 0.000024f;
+    private float cLaton = 0.000020f;
+    private float cCobre = 0.000017f;
+    private float cVidrio = 0.000070f; //Es el promedio
+    private float cInvar = 0.0000009f;
+    private float cCuarzo = 0.000004f;
+    private float cAcero = 0.000012f;
+
+    private float coeficienteActual = 0f;
+    public float lInicial;
+    private float lFinal;
+    public float deltaT;
+    private float deltaL;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GetComponent<Renderer>().material.mainTexture = acero;
+        }
+    }
+
+    //Imagino que numero es el indice del elemento seleccionado de la lista
+    public void cambiarMaterial(int numero)
+    {
+        if (numero == 0)
+        {
+            GetComponent<Renderer>().material.mainTexture = acero;
+        }
+        else if (numero == 1)
+        {
+            GetComponent<Renderer>().material.mainTexture = aluminio;
+        }
+        else if (numero == 2)
+        {
+            GetComponent<Renderer>().material.mainTexture = cobre;
+        }
+        else if (numero == 3)
+        {
+            GetComponent<Renderer>().material.mainTexture = cuarzo;
+        }
+        else if (numero == 4)
+        {
+            GetComponent<Renderer>().material.mainTexture = laton;
+        }
+        else if (numero == 5)
+        {
+            GetComponent<Renderer>().material.mainTexture = vidrio;
+        }
+    }
+}
